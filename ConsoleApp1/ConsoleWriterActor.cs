77a43702308208bebc8a646875a960d742ed28ee
h.fathi <h.fathi@FATHI>
2022-06-26 11:06:43 +0430

    Add project files.

---
+++ ConsoleApp1/ConsoleWriterActor.cs
@@ -0,0 +1,34 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ConsoleWriterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            if (message is Messages.InputError)
+            {
+                var msg = message as Messages.InputError;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(msg.Reason);
+            }
+            else if (message is Messages.InputSuccess)
+            {
+                var msg = message as Messages.InputSuccess;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(msg.Reason);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Akka.Actor;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyActorSystem = ActorSystem.Create("DefaultSystem");
+
+            // create top-level actors within the actor system
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
+
+            // make tailCoordinatorActor
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps,
+                "tailCoordinatorActor");
+
+            // pass tailCoordinatorActor to fileValidatorActorProps (just adding one extra arg)
+            Props fileValidatorActorProps = Props.Create(() =>
+            new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps,
+                "validationActor");
+
+
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            // begin processing
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+}
